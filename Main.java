ea8422d1d1dd3bea244568e097dd940d075ecc26
newtampacperson <cory.person@patelhs.org>
2021-02-01 15:27:48 +0000

    Created question 1 method. Still some modifying to do.

--- Main.java
+++ Main.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
     System.out.println("Math test");
+    int answer = sc.nextInt();
   }
+  static boolean q1(int answer) 
+  {
+    System.out.println("Jimmy and Ariana both want to eat an apple pie. However, they want to share it equally. Jimmy has already ate one pie slice before. The pie has 10 slices. Jimmy says if he eats 4, then it will be fair. Ariana says that if she eats 5, then it woud be fair. Is Ariana right?");
+    System.out.println("1 -- Yes");
+    System.out.println("2 -- No");
+    if(answer == 1)
+    {
+      boolean corAnswer = true;
+      return corAnswer;
+    }
+  //static int q2(int answer) 
+  }
+
 }
